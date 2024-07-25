6a74526dd9f11462935823fb434db107b2b33753
ManishPandey1999 <160839118+ManishPandey1999@users.noreply.github.com>
2024-07-25 13:07:24 +0530

    Update App.jsx
    Add footer and filter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import './App.css';
 import Header from "./components/header";
 import Home from "./components/Home";
+import Footer from './Components/Footer.jsx'
 
 function App() {
   return (
     <>
       <Header />
       <Home />
+      <Footer />
     </>
   );
 }
