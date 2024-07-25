bd8c55dcc8633ca70bbbef2407a924283aaa347e
ManishPandey1999 <160839118+ManishPandey1999@users.noreply.github.com>
2024-07-25 16:22:32 +0530

    Update App.jsx
    React router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css';
-import Header from "./Components/header";
+import Header from "./Components/Header";
 import Home from "./Components/Home";
-import Footer from './Components/Footer.jsx'
+import Footer from './Components/Footer'
+import Search from './Components/Search'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 }
 
